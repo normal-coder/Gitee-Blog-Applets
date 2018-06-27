81de04ef2578d74c82b564377077f4a6e4a99d54
诺墨 <normal@normalcoder.com>
2018-06-28 06:37:06 +0800

    add. 数据库配置文件

---
+++ server/application/Common/Conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: normal
+ * Date: 2016/10/13
+ * Time: 下午4:52
+ */
+
+return array(
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_NAME'   => 'blog_gitee', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '123456', // 密码
+    'DB_PORT'   => '1521', // 端口
+    'DB_PREFIX' => '', // 端口
+);
