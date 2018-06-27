bf8c52910a98895e08c004b335a2bca9d052c482
诺墨 <normal@normalcoder.com>
2018-06-28 06:37:32 +0800

    add. 小程序相关参数配置文件

---
+++ server/application/Common/Conf/miniapp.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: normal
+ * Date: 2016/10/13
+ * Time: 下午4:52
+ */
+
+return array(
+    "miniapp" => array(
+        'appid'  => 'wx271b9731f93d3c20', //小程序appid
+        'secret' => '',                   //小程序appsecret
+    ),
+);
